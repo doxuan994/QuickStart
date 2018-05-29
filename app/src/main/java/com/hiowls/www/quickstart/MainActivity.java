4c1cb7c86aedd66c912cd7c07bcb1ce454e7e114
Xuan Do <doxuancntp@gmail.com>
2018-05-29 14:51:31 -0400

    Commit

--- app/src/main/java/com/hiowls/www/quickstart/MainActivity.java
+++ app/src/main/java/com/hiowls/www/quickstart/MainActivity.java
@@ -2,11 +2,42 @@ package com.hiowls.www.quickstart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
 import android.widget.TextView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG="MainActivity";
+    JavaCameraView javaCameraView;
+    Mat mRgba;
+    Mat imgGray;
+//    Mat imgCanny;
+
+    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS: {
+                    javaCameraView.enableView();
+                    break;
+                }
+                default: {
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+        }
+    };
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -19,22 +50,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
 
-        if (!OpenCVLoader.initDebug()) {
-            tv.setText(tv.getText() + "\nOpenCVLoader.initDebug(), not working");
-        } else {
-            tv.setText(tv.getText() + "\nOpenCVLoader.initDebug(), working");
-            // JNI call
-            tv.setText(tv.getText() + "\n" + validate(0L, 0L));
+        javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (javaCameraView != null) {
+            javaCameraView.disableView();
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (javaCameraView != null) {
+            javaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV successfully loaded");
+            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        } else {
+            Log.d(TAG, "OpenCV not Loaded");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_11, this, mLoaderCallBack);
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        imgGray = new Mat(height, width, CvType.CV_8UC1);
+//        imgCanny = new Mat(height, width, CvType.CV_8UC1);
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+
+        Imgproc.cvtColor(mRgba, imgGray, Imgproc.COLOR_RGB2GRAY);
+//        Imgproc.cvtColor(imgGray, imgCanny, 50, 150);
+
+        return imgGray;
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
-    public native String validate(long matAddrGr, long matAddrRgba);
+//    public native String stringFromJNI();
+//    public native String validate(long matAddrGr, long matAddrRgba);
+
+//    public native void faceDetection(long addrRgba);
 }
